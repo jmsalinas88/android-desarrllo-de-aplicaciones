506440901805b9cc65d4255090b589f45fa113dd
Juan M. Salinas <jmsalinas88@gmail.com>
2020-10-20 08:46:50 -0300

    Agrego Segundo Activity

--- app/src/main/java/mx/unam/petagram/MainActivity.java
+++ app/src/main/java/mx/unam/petagram/MainActivity.java
@@ -7,37 +7,44 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatePickerDialog picker;
-    private EditText eText;
+    private DatePickerDialog datePickerFechaNacimiento;
+    private EditText textFechaNacimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        eText=(EditText) findViewById(R.id.editText1);
-        eText.setInputType(InputType.TYPE_NULL);
+        textFechaNacimiento =(EditText) findViewById(R.id.editText1);
+        textFechaNacimiento.setInputType(InputType.TYPE_NULL);
 
-        eText.setOnClickListener(new View.OnClickListener() {
+        textFechaNacimiento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Calendar cldr = Calendar.getInstance();
                 int day = cldr.get(Calendar.DAY_OF_MONTH);
-                int month = cldr.get(Calendar.MONTH);
+                int month = cldr.get(Calendar.MONTH) + 1;
                 int year = cldr.get(Calendar.YEAR);
-                // date picker dialog
-                picker = new DatePickerDialog(MainActivity.this,
+                datePickerFechaNacimiento = new DatePickerDialog(MainActivity.this,
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                String dateStr = "";
+                                if(dayOfMonth < 10){
+                                    dateStr = "0" + dayOfMonth + "/" + monthOfYear + "/" + year;
+                                }else{
+                                    dateStr = dayOfMonth + "/" + monthOfYear + "/" + year;
+                                }
+                                textFechaNacimiento.setText(dateStr);
                             }
                         }, year, month, day);
-                picker.show();
+                datePickerFechaNacimiento.show();
             }
         });
     }
