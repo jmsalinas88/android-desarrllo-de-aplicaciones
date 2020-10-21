23f8de2b8a328300684b77bf06c7543779d2e3af
Juan M. Salinas <jmsalinas88@gmail.com>
2020-10-21 09:00:25 -0300

    Avances Segunda Pantalla

--- app/src/main/java/mx/unam/petagram/MainActivity.java
+++ app/src/main/java/mx/unam/petagram/MainActivity.java
@@ -2,27 +2,50 @@ package mx.unam.petagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-
-import java.time.LocalDate;
-import java.time.Month;
+import com.google.android.material.textfield.TextInputEditText;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatePickerDialog datePickerFechaNacimiento;
     private EditText textFechaNacimiento;
+    private TextInputEditText textNombre;
+    private TextInputEditText textTelefono;
+    private TextInputEditText textEmail;
+    private EditText textDescContacto;
+    private Button miBotonSiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textFechaNacimiento =(EditText) findViewById(R.id.editText1);
+        textFechaNacimiento =(EditText) findViewById(R.id.textFxNacimiento);
         textFechaNacimiento.setInputType(InputType.TYPE_NULL);
+        textNombre =(TextInputEditText) findViewById(R.id.textNombre);
+        textTelefono =(TextInputEditText) findViewById(R.id.textTelefono);
+        textEmail =(TextInputEditText) findViewById(R.id.textEmail);
+        textDescContacto =(TextInputEditText) findViewById(R.id.textDescContacto);
+        miBotonSiguiente =(Button) findViewById(R.id.miBotonSiguiente);
+
+        Bundle parametros = savedInstanceState;
+        String nombre = parametros.getString(getResources().getString(R.string.NOMBRE));
+        String fxNacimiento = parametros.getString(getResources().getString(R.string.FXNACIMIENTO));
+        String telefono = parametros.getString(getResources().getString(R.string.TELEFONO));
+        String email = parametros.getString(getResources().getString(R.string.EMAIL));
+        String descContacto = parametros.getString(getResources().getString(R.string.DESC_CONTACTO));
+
+        textNombre.setText(nombre);
+        textFechaNacimiento.setText(fxNacimiento);
+        textTelefono.setText(telefono);
+        textEmail.setText(email);
+        textDescContacto.setText(descContacto);
 
         textFechaNacimiento.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +68,20 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }, year, month, day);
                 datePickerFechaNacimiento.show();
+            }
+        });
+
+        miBotonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra(getResources().getString(R.string.NOMBRE), textNombre.getText().toString());
+                intent.putExtra(getResources().getString(R.string.FXNACIMIENTO),textFechaNacimiento.getText().toString());
+                intent.putExtra(getResources().getString(R.string.TELEFONO),textTelefono.getText().toString());
+                intent.putExtra(getResources().getString(R.string.EMAIL),textEmail.getText().toString());
+                intent.putExtra(getResources().getString(R.string.DESC_CONTACTO),textDescContacto.getText().toString());
+                startActivity(intent);
+                finish();
             }
         });
     }
