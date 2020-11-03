e74d532d1f6d69a7b9af077860e1e7df4e2d8efb
Juan M. Salinas <jmsalinas88@gmail.com>
2020-11-02 23:56:47 -0300

    Avances semana 3

--- app/src/main/java/mx/unam/petagram/MainActivity.java
+++ app/src/main/java/mx/unam/petagram/MainActivity.java
@@ -1,8 +1,9 @@
 package mx.unam.petagram;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar miActionBar = (Toolbar)findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
     }
 }
